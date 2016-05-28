5ed9adae2d4a83f6a25108e9e41474e0ae6d1752
Rotem Ruso <rotemr@similarweb.com>
2016-05-28 15:36:56 +0300

    Renewed

--- scripts/info.controller.js
+++ scripts/info.controller.js
@@ -3,11 +3,14 @@
  */
 app.controller('infoController', ['$scope', '$http', function ($scope, $http) {
 
-    $scope.info = {};
+    // todo: receive an id from a query string (?) and trying to filter the information by it
+
+    $scope.infos = [];
 
     $http.get("info.json").then(function (response) {
-        $scope.info = response.data.query_result.data.rows[0];
+        $scope.infos = response.data.query_result.data.rows;
     });
-
-
 }]);
+
+
+// fixme: http://data.obudget.org/api/queries/497/results/134030.json?api_key=5SbTE9eOrV2oqfAI0ybXyNvBaYiVdoCzDO4EpEVK
