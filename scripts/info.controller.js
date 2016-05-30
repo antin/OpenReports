028c0e9da454cb9d6a881d04d8b6ec6c92762019
antin <alon.antin@gmail.com>
2016-05-30 22:25:35 +0300

    Update info.controller.js
    filter the data in the backend

--- scripts/info.controller.js
+++ scripts/info.controller.js
@@ -4,7 +4,37 @@
 app.controller('infoController', ['$scope', '$http', function ($scope, $http) {
 
     $scope.info = {};
+	$scope.calc = {};
+	var mk_id = 952 ; // יעל כהן פארן"
+	// read from query string
 
+	//$http.get('http://data.obudget.org/api/queries/528/results/135177.json?api_key=5SbTE9eOrV2oqfAI0ybXyNvBaYiVdoCzDO4EpEVK')
+	$http.get('data/135177.json')
+		.success(function(data) {
+			 //$scope.zipCodes = data;
+			 // filter by mk_id
+			 //$scope.info.votes = data.query_result.data.rows[1].time_calc;
+			 $scope.info_rows = data.query_result.data.rows;//[1].time_calc;
+			 var rawData = data.query_result.data.rows;
+			 angular.forEach(rawData, function (value) {
+				 
+				 if (value.mk_id != mk_id) {
+					 return;
+				 }
+				 console.log(value.category);
+				 console.log(value.time_calc);
+				 $scope.calc[value.category] = value.time_calc;
+				 console.log($scope.calc[value.category]);
+			 });
+			
+			 
+		})
+		.error(function(error, status, headers, config) {
+			 console.log(status);
+			 console.log("Error occured");
+		});
+
+		/*
     $http.get("http://data.obudget.org/api/queries/497/results/134030.json?api_key=5SbTE9eOrV2oqfAI0ybXyNvBaYiVdoCzDO4EpEVK").then(function (response) {
 
         var votes =0;
@@ -33,7 +63,8 @@ app.controller('infoController', ['$scope', '$http', function ($scope, $http) {
         end.public_help=public_help;
 
         $scope.info = end;
+		
     });
-
+*/
 
 }]);
